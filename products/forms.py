d4a5bf4f066731f3bbb821fc2eeff22f2ebd0e5e
sophieboyle1 <sophieboylemiami@gmail.com>
2021-02-27 10:36:51 +0000

    product admin - add product view, url and template

--- products/forms.py
+++ products/forms.py
@@ -1,34 +1,18 @@
 from django import forms
-from .models import UserProfile
+from .models import Product, Category
 
 
-class UserProfileForm(forms.ModelForm):
+class ProductForm(forms.ModelForm):
+
     class Meta:
-        model = UserProfile
-        exclude = ('user',)
+        model = Product
+        fields = '__all__'
 
     def __init__(self, *args, **kwargs):
-        """
-        Add placeholders and classes, remove auto-generated
-        labels and set autofocus on first field
-        """
         super().__init__(*args, **kwargs)
-        placeholders = {
-            'default_phone_number': 'Phone Number',
-            'default_postcode': 'Postal Code',
-            'default_town_or_city': 'Town or City',
-            'default_street_address1': 'Street Address 1',
-            'default_street_address2': 'Street Address 2',
-            'default_county': 'County, State or Locality',
-        }
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
-        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
-        for field in self.fields:
-            if field != 'default_country':
-                if self.fields[field].required:
-                    placeholder = f'{placeholders[field]} *'
-                else:
-                    placeholder = placeholders[field]
-                self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
-            self.fields[field].label = False
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
